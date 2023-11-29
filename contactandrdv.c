760d51af301a3c121b9fdeecfcd6c04671dc7d0c
alban <alban.sulpice@efrei.net>
2023-11-29 13:31:16 +0100

    fct recherche/complétion

--- contactandrdv.c
+++ contactandrdv.c
@@ -27,3 +27,34 @@ char *contactDatasToSimpleString(char *firstname, char *lastname) {
     snprintf(result, length, "%s_%s", lastname, firstname);
     return result;
 }
+
+void rechercherContact(AgendaEntry *agenda, char *partialName, int niveau) {
+    if (agenda == NULL) {
+        printf("Aucun contact trouvé.\n");
+        return;
+    }
+
+    // Comparer la première lettre du nom du contact avec la lettre actuelle
+    char currentLetter = partialName[niveau];
+    char *contactName = agenda->contact.nom;
+
+    if (contactName[niveau] == currentLetter) {
+        // Si on atteint la 3ème lettre, afficher le nom complet
+        if (niveau == 2) {
+            printf("%s %s\n", agenda->contact.nom, agenda->contact.prenom);
+        }
+        // Passer au niveau suivant
+        rechercherContact(agenda, partialName, niveau + 1);
+    } else {
+        // Si la lettre ne correspond pas, passer au contact suivant
+        rechercherContact(agenda->suivant, partialName, 0);
+    }
+}
+
+// Fonction principale pour rechercher un contact
+void rechercheContactMenu(AgendaEntry *agenda) {
+    printf("Entrez le nom à rechercher (3 lettres minimum) : ");
+    char *partialName = scanString();
+    rechercherContact(agenda, partialName, 0);
+    free(partialName); // Libérer la mémoire allouée pour la saisie
+}
