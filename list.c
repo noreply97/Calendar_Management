21d4f5d5a8725554aa01d6301e00bc357d6022bc
noreply97 <115534973+noreply97@users.noreply.github.com>
2025-07-03 09:31:59 +0200

    Update list.c

--- list.c
+++ list.c
@@ -4,23 +4,85 @@
 
 #include "list.h"
 
-List* createList(int nbLevels){
-    List* list = (List*)malloc(sizeof(List));
-    list->max_level = nbLevels;
-    return list;
+List *createEmptyList(int maxLevel) {
+    List *mylist = (List *) malloc(sizeof(List));
+    mylist->maxLevel = maxLevel;
+    mylist->heads = (Cell **) malloc(sizeof(Cell *)*maxLevel);
+    for (int i = 0; i < maxLevel; i++) {
+        mylist->heads[i] = NULL;
+    }
+    return mylist;
+}//fonctionne
+
+int isEmptyList(List *myList) {
+    int res = 0;
+    for (int i = 0; i < myList->maxLevel; i++) {
+        if(myList->heads[i] == NULL){
+            res += 1;
+        }
+    }
+    if(res == myList->maxLevel){
+        return 1;
+    } else {
+        return 0;
+    }
+}//fonctionne
+
+
+void insertCellAtHead(List *mylist, Cell *newCell) {
+    if (!isEmptyList(mylist)) {
+        for (int i = 0; i < newCell->nbLevel; i++) {
+            newCell->nexts[i] = mylist->heads[i];
+            mylist->heads[i] = newCell;
+        }
+    }
+    printf("Liste vide");
 }
 
-void addCellToHeadList(Cell* cell, List* list){
-    for(int i=0; i<cell->max_level;i++){
-        cell->nexts[i] = list->heads[i];
-    }
-    for(int i=0; i<cell->max_level;i++){
-        list->heads[i] = cell;
+//void addCellAtHead(List *mylist, Cell *cell) {
+//    if (!isEmptyList(mylist)) {
+//        cell->nexts[0] = mylist->heads[0];
+//        mylist->heads[0] = cell;
+//    }
+//}
+
+//void printListLevel(List *mylist, int nbLevel) {
+//    if (isEmptyList(mylist)) {
+//        printf("Vide");
+//    } else {
+//        if (nbLevel <= mylist->maxLevel) {
+//            printf("[list head_%d-]-->", nbLevel);
+//            Cell *temp = mylist->heads[nbLevel];
+//            while (temp != NULL) {
+//                printf("%d",temp->value);
+//                temp = temp->nexts[nbLevel];
+//            }
+//            printf("NULL");
+//        }
+//    }
+//}
+
+void printList(List *mylist) {
+    for (int i = 0; i < mylist->maxLevel; ++i) {
+        printListLevel(mylist, i);
     }
 }
 
-void displayListCellsFromLevel(List list, int level){
-    printf("[list head_%d @-]-->", level-1);
+void deleteList(List *mylist) {
+    free(mylist);
+}
 
+void printListLevel(List *mylist, int Level) {
+    if (!isEmptyList(mylist)) {
+        Cell *temp = mylist->heads[Level];
+        printf("[list head_%d-]-->", Level);
+        while (temp->nexts[Level] != NULL){
+            printf("[ %d|@-]-->",temp->value);
+            temp=temp->nexts[Level];
+        }
+        printf("-->NULL");
+    } else{
+        printf("Liste vide");
+    }
 
 }
