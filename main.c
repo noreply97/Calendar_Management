54933a4a840dc5312a5090915ed4875e10553e62
noreply97 <115534973+noreply97@users.noreply.github.com>
2025-07-03 09:29:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "Cell.h"
+#include "Level_List.h"
 
 int main() {
-    printf("Hello, World!\n");
+    Level_list *mylist = createEmptyLevel_list(2);
+    isEmptyList(mylist);
+    //printListLevel(mylist,2)
+    //Cell *cell = createCell(2,2);
+    //printCell(cell);
+//    insertCellAtHead(mylist,cell);
+//    //addCellAtHead(mylist,cell);
+//    printListLevel(mylist, 0);
     return 0;
 }
